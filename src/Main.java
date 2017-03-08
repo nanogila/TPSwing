011ef0cc8180394c1cf528719e5d6df67b5aa18c
tomasvarela <tomas.varela@ing.austral.edu.ar>
2017-03-07 23:13:04 -0300

    added main window

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,7 @@
-import javax.swing.*;
 
-import controller.MainController;
-import model.Model;
-import view.*;
 public class Main {
     public static void main(String[] args){
-		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); //tell Swing to display GUI components according to the OS style
-			new MainController(new Model());
-		} catch (Exception e) { //need to handle .setLookAndFeel exceptions
-			e.printStackTrace();
-		}
+        MainWindow aWindow = new MainWindow();
+        aWindow.createandShowGUI();
     }
 }
